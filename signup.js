290b26709133f18dc5b9ca9701894652264ecba3
dhruv10032808 <dhruv.bhanu.1003@gmail.com>
2023-04-02 13:02:20 +0530

    signup frontend

---
+++ signup.js
@@ -0,0 +1,22 @@
+const form=document.getElementById('form');
+const name=document.getElementById('name');
+const email=document.getElementById('email');
+const phone=document.getElementById('phone')
+const password=document.getElementById('password');
+
+form.addEventListener('submit',signup);
+
+function signup(event){
+    event.preventDefault();
+    const nameValue=event.target.name.value;
+    const emailValue=event.target.email.value;
+    const phoneValue=event.target.phone.value
+    const passwordValue=event.target.password.value;
+    const data={
+      name:nameValue,
+      email:emailValue,
+      phone:phoneValue,
+      password:passwordValue
+    }
+    axios.post('http://localhost:3000/signup',data)
+}
