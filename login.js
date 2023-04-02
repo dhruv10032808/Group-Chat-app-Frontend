97c8990c0abb3e5d12e4be6b74a4eefaaf284126
dhruv10032808 <dhruv.bhanu.1003@gmail.com>
2023-04-02 14:22:32 +0530

    login frontend

---
+++ login.js
@@ -0,0 +1,20 @@
+const form=document.getElementById('form');
+const email=document.getElementById('email');
+const password=document.getElementById('password');
+
+form.addEventListener('submit',login);
+
+function login(event){
+    event.preventDefault();
+    const emailValue=event.target.email.value;
+    const passwordValue=event.target.password.value;
+    const data={
+      email:emailValue,
+      password:passwordValue
+    }
+    axios.post('http://localhost:3000/login',data).then(res=>{
+        alert(res.data.newUserDetail.message);
+    }).catch(err=>{
+        document.body.innerHTML+=`<div style="color:red">${err.message} </div>`
+    })
+}
