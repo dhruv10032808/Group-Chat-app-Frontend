524fb0bc4ff58c393a37dc41d69af5e0dd34ac52
dhruv10032808 <dhruv.bhanu.1003@gmail.com>
2023-04-03 15:28:28 +0530

    optimized chat app frontend

--- chat.js
+++ chat.js
@@ -15,25 +15,44 @@ function send(e){
     })
 }
 
-window.addEventListener('DOMContentLoaded',getmessage)
+window.addEventListener('DOMContentLoaded',fetchMessageFromLocal)
 
 setInterval(()=>{
     getmessage()
-},1000);
+},10000);
 
 function getmessage(){
-    document.getElementById('msg').innerHTML='';
-    axios.get('http://localhost:3000/message',{headers:{'Authorization':token}}).then(response=>{
-        for(let i=0;i<response.data.length;i++){
-            viewmessage(response.data[i])
-        }
+    let message=[];
+    if(localStorage.hasOwnProperty('message')==='true'){
+        message=JSON.parse(localStorage.getItem('message'))
+        var lastmsgid=message[message.length-1].id
+    }
+    //document.getElementById('msg').innerHTML='';
+    axios.get(`http://localhost:3000/message/:${lastmsgid}`,{headers:{'Authorization':token}}).then(response=>{
+        // for(let i=0;i<response.data.length;i++){
+        //     viewmessage(response.data[i])
+        // }
+        console.log(response.data)
+        var arr=[...message,...response.data.message];
+        var msg=JSON.stringify(arr);
+        //console.log(msg);
+        localStorage.setItem('message',msg);
+        fetchMessageFromLocal()
     }).catch(err=>{
         console.log(err);
     })
 }
 
+function fetchMessageFromLocal(){
+    document.getElementById('msg').innerHTML="";
+    const msg=JSON.parse(localStorage.getItem('message'));
+    console.log(msg);
+        for(var i=0;i<msg.length;i++){
+            viewmessage(msg[i]);
+        }
+}
+
 function viewmessage(msg){
-    const parentnode=document.getElementById('msg')
-    const childhtml=`<h5>${msg.message}</h5>`;
-    parentnode.innerHTML=parentnode.innerHTML+childhtml;
+    console.log(msg)
+    document.getElementById('msg').innerHTML+=`<h5>${msg.message}</h5>`
 }
