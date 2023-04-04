513d24f31b846962dfdf77712915ad9817561b9a
dhruv10032808 <dhruv.bhanu.1003@gmail.com>
2023-04-04 15:39:20 +0530

    group features frontend

--- chat.js
+++ chat.js
@@ -1,15 +1,19 @@
 const message=document.getElementById('message');
 const button=document.getElementById('btn');
 const token=localStorage.getItem('token');
+const form1=document.getElementById('form1');
+const form2=document.getElementById('form2');
+
 button.addEventListener('click',send)
 
 function send(e){
+    const gid=localStorage.getItem('gid')
     e.preventDefault();
     const data={
         message:message.value
     }
     console.log(data)
-    axios.post('http://localhost:3000/message',data,{headers:{'Authorization':token}}).then(res=>{
+    axios.post(`http://localhost:3000/message?gid=${gid}`,data,{headers:{'Authorization':token}}).then(res=>{
         console.log(res.data)
         viewmessage(res.data)
     })
@@ -17,9 +21,11 @@ function send(e){
 
 window.addEventListener('DOMContentLoaded',fetchMessageFromLocal)
 
-setInterval(()=>{
-    getmessage()
-},10000);
+window.addEventListener('DOMContentLoaded',getgroup)
+
+// setInterval(()=>{
+//     getmessage()
+// },1000);
 
 function getmessage(){
     let message=[];
@@ -27,15 +33,9 @@ function getmessage(){
         message=JSON.parse(localStorage.getItem('message'))
         var lastmsgid=message[message.length-1].id
     }
-    //document.getElementById('msg').innerHTML='';
-    axios.get(`http://localhost:3000/message/:${lastmsgid}`,{headers:{'Authorization':token}}).then(response=>{
-        // for(let i=0;i<response.data.length;i++){
-        //     viewmessage(response.data[i])
-        // }
-        console.log(response.data)
+    axios.get(`http://localhost:3000/message/${lastmsgid}`,{headers:{'Authorization':token}}).then(response=>{
         var arr=[...message,...response.data.message];
         var msg=JSON.stringify(arr);
-        //console.log(msg);
         localStorage.setItem('message',msg);
         fetchMessageFromLocal()
     }).catch(err=>{
@@ -44,15 +44,61 @@ function getmessage(){
 }
 
 function fetchMessageFromLocal(){
-    document.getElementById('msg').innerHTML="";
+    document.getElementById('chat').innerHTML="";
     const msg=JSON.parse(localStorage.getItem('message'));
-    console.log(msg);
         for(var i=0;i<msg.length;i++){
             viewmessage(msg[i]);
         }
 }
 
 function viewmessage(msg){
-    console.log(msg)
-    document.getElementById('msg').innerHTML+=`<h5>${msg.message}</h5>`
+    document.getElementById('chat').innerHTML+=`<p>${msg.message}</p>`
+}
+
+form1.addEventListener('submit',creategroup)
+
+function creategroup(e){
+    e.preventDefault();
+    console.log(e.target.group.value)
+    const data={
+        group:e.target.group.value
+    }
+    axios.post('http://localhost:3000/creategroup',data,{headers:{'Authorization':token}}).then(res=>{
+        console.log(res)
+    }).catch(err=>console.log(err));
+}
+
+function getgroup(){
+    axios.get('http://localhost:3000/getgroup',{headers:{'Authorization':token}}).then(response=>{
+        response.data.group.forEach(group=>{
+            const list=document.getElementById('grouplist');
+            const childhtml=`<a onclick="getgroupmessage('${group.id}')" style="cursor:pointer;">${group.groupname}</a>`
+            list.innerHTML+=childhtml;
+        })
+    })
+}
+
+function getgroupmessage(gid){
+    document.getElementById('chat').innerHTML='';
+    localStorage.setItem('gid',gid);
+    axios.get(`http://localhost:3000/getgroupmessage/?gid=${gid}`,{headers:{"Authorization":token}}).then(res=>{
+        res.data.message.forEach(message=>{
+            viewmessage(message)
+        })
+    })
+}
+
+form2.addEventListener('submit',joingroup);
+
+function joingroup(e){
+    e.preventDefault();
+    const gid=localStorage.getItem('gid')
+    console.log(e.target.useremail.value);
+    const data={
+        email:e.target.useremail.value,
+        gid:gid
+    }
+    axios.post('http://localhost:3000/joingroup',data,{headers:{"Authorization":token}}).then(response=>{
+        alert(response.data.message)
+    }).catch(err=>console.log(err));
 }
